a4b19f47011f81ae78ba15aa308b92e2515f1c79
Sebastian Hildebrandt <hildebrandt@plus-innovations.com>
2021-03-18 21:13:16 +0100

    feat: added validators

--- dist/validators.d.ts
+++ dist/validators.d.ts
@@ -1,11 +1,17 @@
 declare const validator: {
+    isEmail(str: string): boolean;
+    isUrl(str: string): boolean;
+    isIP(str: string): 0 | 4 | 6;
+    isIPv4(str: string): boolean;
+    isIPv6(str: string): boolean;
+    isIPNet(str: string): boolean;
+    isAlpha(str: string): boolean;
+    isAlphanumeric(str: string): boolean;
     isNumeric(str: string): boolean;
     isNumber(n: any): boolean;
     isHex(str: string): boolean;
     isInt(str: string): boolean;
     isDecimal(str: string): false | RegExpMatchArray | null;
     isDate(str: string): boolean;
-    isAlpha(str: string): boolean;
-    isAlphanumeric(str: string): boolean;
 };
 export default validator;
--- dist/validators.js
+++ dist/validators.js
@@ -1,6 +1,50 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 const validator = {
+    isEmail(str) {
+        return (str.match(/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/) !== null);
+        ;
+    },
+    isUrl(str) {
+        return str.length < 2083 && (str.match(/^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i) !== null);
+    },
+    isIP(str) {
+        if (!str) {
+            return 0;
+        }
+        else if (/^(\d?\d?\d)\.(\d?\d?\d)\.(\d?\d?\d)\.(\d?\d?\d)$/.test(str)) {
+            const parts = str.split('.');
+            for (const part of parts) {
+                const partInt = parseInt(part, 10);
+                if (partInt < 0 || 255 < partInt) {
+                    return 0;
+                }
+            }
+            return 4;
+        }
+        else if (/^::|^::1|^([a-fA-F0-9]{1,4}::?){1,7}([a-fA-F0-9]{1,4})$/.test(str)) {
+            return 6;
+        }
+        else {
+            return 0;
+        }
+    },
+    isIPv4(str) {
+        return validator.isIP(str) === 4;
+    },
+    isIPv6(str) {
+        return validator.isIP(str) === 6;
+    },
+    isIPNet(str) {
+        return validator.isIP(str) !== 0;
+    },
+    isAlpha(str) {
+        return (str.match(/^[a-zA-Z]+$/) !== null);
+        ;
+    },
+    isAlphanumeric(str) {
+        return (str.match(/^[a-zA-Z0-9]+$/) !== null);
+    },
     isNumeric(str) {
         return (str.match(/^-?[0-9]+$/) !== null);
     },
@@ -27,12 +71,6 @@ const validator = {
     isDate(str) {
         const intDate = Date.parse(str);
         return !isNaN(intDate);
-    },
-    isAlpha(str) {
-        return (str.match(/^[a-zA-Z]+$/) !== null);
-    },
-    isAlphanumeric(str) {
-        return (str.match(/^[a-zA-Z0-9]+$/) !== null);
     }
 };
 exports.default = validator;
